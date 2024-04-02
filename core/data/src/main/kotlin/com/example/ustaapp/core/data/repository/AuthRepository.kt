531d903261d6f240a4b9704eaaf552ca51621598
Abdulaziz Abduqodirov <abdulazizabduqodirov@outlook.com>
2024-04-02 16:08:48 +0500

    Persists token pair upon signing in

--- core/data/src/main/kotlin/com/example/ustaapp/core/data/repository/AuthRepository.kt
+++ core/data/src/main/kotlin/com/example/ustaapp/core/data/repository/AuthRepository.kt
@@ -3,6 +3,7 @@ package com.example.ustaapp.core.data.repository
 import com.example.ustaapp.core.network.Dispatcher
 import com.example.ustaapp.core.network.UstaDispatchers.IO
 import com.example.ustaapp.core.network.UstaNetworkDataSource
+import com.example.ustaapp.core.network.token.TokenManager
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -10,6 +11,7 @@ import javax.inject.Inject
 class AuthRepository @Inject constructor(
     private val ustaNetworkDataSource: UstaNetworkDataSource,
     @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
+    private val tokenManager: TokenManager,
 ) {
     suspend fun requestVerificationCode(phoneNumber: String) =
         withContext(ioDispatcher) {
@@ -18,6 +20,8 @@ class AuthRepository @Inject constructor(
 
     suspend fun confirmVerificationCode(phoneNumber: String, code: String) =
         withContext(ioDispatcher) {
-            ustaNetworkDataSource.confirmVerificationCode(phoneNumber, code)
+            val tokenPair = ustaNetworkDataSource.confirmVerificationCode(phoneNumber, code)
+            tokenManager.saveAccessToken(tokenPair.accessToken)
+            tokenManager.saveRefreshToken(tokenPair.refreshToken)
         }
 }
